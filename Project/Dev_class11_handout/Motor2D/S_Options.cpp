0e120af6d10d672e8e2ac432877a665df9d58b3b
xavierolivenza <yotolivenza@gmail.com>
2017-03-29 10:34:29 +0200

    Options menu buttons moved

--- Project/Dev_class11_handout/Motor2D/S_Options.cpp
+++ Project/Dev_class11_handout/Motor2D/S_Options.cpp
@@ -2,6 +2,7 @@
 #include "Gui.h"
 #include "GuiLabel.h"
 #include "GuiButton.h"
+#include "j1Window.h"
 
 S_Options::S_Options()
 {
@@ -13,22 +14,23 @@ S_Options::~S_Options()
 
 bool S_Options::Start()
 {
-	OptionsLabel = App->gui->CreateLabel(iPoint(300, 100), &std::string("Options menu"), false);
+	OptionsLabel = App->gui->CreateLabel(iPoint(200, 100), &std::string("Options menu"), false);
 	OptionsLabel->SetFont(App->font->Sherwood28);
 	((Gui*)OptionsLabel)->SetListener(this);
-	controls = App->gui->CreateButton(iPoint(1000, 370), &std::string("Controls Settings"), ButtonType::idle_hover_pressed, &idle_button_rect, &hover_button_rect, &pressed_button_rect, false);
+	int X_pos = App->win->GetWindowWHalf() - (int)(idle_button_rect.w * 0.5f);
+	controls = App->gui->CreateButton(iPoint(X_pos, 200), &std::string("Controls Settings"), ButtonType::idle_hover_pressed, &idle_button_rect, &hover_button_rect, &pressed_button_rect, false);
 	controls->SetFont(App->font->Sherwood20);
 	((Gui*)controls)->SetListener(this);
-	video = App->gui->CreateButton(iPoint(1000, 450), &std::string("Video Settings"), ButtonType::idle_hover_pressed, &idle_button_rect, &hover_button_rect, &pressed_button_rect, false);
+	video = App->gui->CreateButton(iPoint(X_pos, 310), &std::string("Video Settings"), ButtonType::idle_hover_pressed, &idle_button_rect, &hover_button_rect, &pressed_button_rect, false);
 	video->SetFont(App->font->Sherwood20);
 	((Gui*)video)->SetListener(this);
-	audio = App->gui->CreateButton(iPoint(1000, 530), &std::string("Audio Settings"), ButtonType::idle_hover_pressed, &idle_button_rect, &hover_button_rect, &pressed_button_rect, false);
+	audio = App->gui->CreateButton(iPoint(X_pos, 420), &std::string("Audio Settings"), ButtonType::idle_hover_pressed, &idle_button_rect, &hover_button_rect, &pressed_button_rect, false);
 	audio->SetFont(App->font->Sherwood20);
 	((Gui*)audio)->SetListener(this);
-	gameplay = App->gui->CreateButton(iPoint(1000, 610), &std::string("Gameplay Settings"), ButtonType::idle_hover_pressed, &idle_button_rect, &hover_button_rect, &pressed_button_rect, false);
+	gameplay = App->gui->CreateButton(iPoint(X_pos, 530), &std::string("Gameplay Settings"), ButtonType::idle_hover_pressed, &idle_button_rect, &hover_button_rect, &pressed_button_rect, false);
 	gameplay->SetFont(App->font->Sherwood20);
 	((Gui*)gameplay)->SetListener(this);
-	back = App->gui->CreateButton(iPoint(500, 610), &std::string("Back"), ButtonType::idle_hover_pressed, &idle_button_rect, &hover_button_rect, &pressed_button_rect, false);
+	back = App->gui->CreateButton(iPoint(920, 600), &std::string("Back"), ButtonType::idle_hover_pressed, &idle_button_rect, &hover_button_rect, &pressed_button_rect, false);
 	back->SetFont(App->font->Sherwood20);
 	((Gui*)back)->SetListener(this);
 
