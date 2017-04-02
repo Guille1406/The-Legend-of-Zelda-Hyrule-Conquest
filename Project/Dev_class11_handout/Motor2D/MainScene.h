0a1ab577074f2f3692ed112fdb7d146ca28e6989
xavierolivenza <yotolivenza@gmail.com>
2017-04-02 19:28:11 +0200

    Menu working with controller

--- Project/Dev_class11_handout/Motor2D/MainScene.h
+++ Project/Dev_class11_handout/Motor2D/MainScene.h
@@ -81,7 +81,7 @@ protected:
 
 	void MenuInput(std::vector<GuiButton*>* buttons)
 	{
-		if (App->input->GetKey(SDL_SCANCODE_UP) == KEY_DOWN)
+		if ((App->input->GetKey(SDL_SCANCODE_UP) == KEY_DOWN) || (App->input->GetControllerButton(0, 11) == KEY_DOWN))
 		{
 			std::vector<GuiButton*>::iterator focused_button = std::find(buttons->begin(), buttons->end(), App->gui->GetFocus());
 			if ((*focused_button) != buttons->front())
@@ -91,7 +91,7 @@ protected:
 			}
 		}
 
-		if (App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_DOWN)
+		if ((App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_DOWN) || (App->input->GetControllerButton(0, 12) == KEY_DOWN))
 		{
 			std::vector<GuiButton*>::iterator focused_button = std::find(buttons->begin(), buttons->end(), App->gui->GetFocus());
 			if ((*focused_button) != buttons->back())
