bcc50b79c44f0176d1b5ffa2c081ae33521748e4
mizquierdo97 <mizquierdo97@gmail.com>
2017-05-06 21:55:15 +0200

    Heart Container header added

--- Project/Dev_class11_handout/Motor2D/O_HeartContainer.h
+++ Project/Dev_class11_handout/Motor2D/O_HeartContainer.h
@@ -1,1 +1,47 @@
-#pragma once
+#ifndef _HEARTCONTAINER_
+#define _HEARTCONTAINER_
+#include "j1Object.h"
+#include"j1Render.h"
+#include "Character.h"
+class HeartContainer : public Object {
+private:
+	bool triggered = false;
+public:
+	HeartContainer() {
+
+	}
+	HeartContainer(const HeartContainer& obj) {
+		type = obj.type;
+		rect = obj.rect;
+		active = obj.active;
+		name = obj.name;
+		logic_height = obj.logic_height;
+		//texture_rect = obj.texture_rect;
+	}
+	~HeartContainer() {
+
+	}
+	void Pick(Character* player) {
+		/*
+		if (App->player->half_hearts_test_purpose == (App->player->hearts_containers_test_purpose * 2) - 1) {
+			App->player->half_hearts_test_purpose++;
+		}
+		else if ((App->player->half_hearts_test_purpose / 2) <= (App->player->hearts_containers_test_purpose - 1)) {
+			App->player->half_hearts_test_purpose += 2;
+		}
+
+		tokill = true;*/
+	}
+
+
+
+public:
+
+
+
+
+};
+
+
+#endif
+
